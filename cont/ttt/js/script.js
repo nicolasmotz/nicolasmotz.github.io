90a953043e67af804a2a5abc0bedff7aa83a9f2f
nicolasmotz <nicolas.motz@gmail.com>
2018-09-09 11:12:05 +0200

    Started Tic Tac Toe project

---
+++ cont/ttt/js/script.js
@@ -0,0 +1,41 @@
+var Board;
+const huPlayer = -1;
+const aiPlayer = 1;
+const cells = document.querySelectorAll('.cell');
+
+loadTheta();
+startGame();
+
+
+function loadTheta() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", "theta.txt", true);
+  xhttp.send();
+}
+
+function startGame() {
+    for (var i = 0; i < cells.length; i++) {
+		cells[i].innerText = '';
+		cells[i].addEventListener('click', turnClick, false);
+        Board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    }
+}
+
+function turnClick(square) {
+	turn(square.target.id, huPlayer)
+}
+
+function turn(squareId, player) {
+	Board[squareId] = player;
+    if (player == -1) {
+        document.getElementById(squareId).innerText = 'X';
+    } else {
+        document.getElementById(squareId).innerText = 'O';
+    }
+}

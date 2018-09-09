9678a55ebdb4407ff0d14e82580b7b5e6f692bc6
nicolasmotz <nicolas.motz@gmail.com>
2018-09-09 11:19:13 +0200

    minor change

--- cont/ttt/js/script.js
+++ cont/ttt/js/script.js
@@ -11,11 +11,11 @@ function loadTheta() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML =
+      document.getElementById('demo').innerText =
       this.responseText;
     }
   };
-  xhttp.open("GET", "theta.txt", true);
+  xhttp.open('GET', 'theta.txt', true);
   xhttp.send();
 }
 
